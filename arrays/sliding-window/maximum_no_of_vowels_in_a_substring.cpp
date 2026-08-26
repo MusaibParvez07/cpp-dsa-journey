228a2f749547d3873f3e398566cc9d4c5bcfbbae
Musaib Parveez <parvezmusaib2@gmail.com>
2026-08-26 16:47:52 +0530

    maximum_no_of_vowels_in_a_substring.cpp

--- arrays/sliding-window/maximum_no_of_vowels_in_a_substring.cpp
+++ arrays/sliding-window/maximum_no_of_vowels_in_a_substring.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cctype>
 using namespace std;
 
 bool isVowel(char ch)
 {
+    ch = tolower(ch);
     return ch == 'a' || ch == 'e' ||
            ch == 'i' || ch == 'o' ||
            ch == 'u';
@@ -12,9 +14,20 @@ bool isVowel(char ch)
 
 int main()
 {
-    string str = "abciiidef";
+    string str;
+    cout << "Enter the String: ";
+    getline(cin, str);
 
-    int k = 3;
+    int k;
+    cout << "Enter the window size (k): ";
+    cin >> k;
+
+    if (k > str.size() || k <= 0)
+    {
+        cout << "Invalid window size" << endl;
+        return 0;
+    }
+
     int vowelCount = 0;
     int maxVowels = 0;
 
@@ -34,6 +47,9 @@ int main()
     cout << "Maximum Vowels: " << maxVowels << endl;
     return 0;
 }
+
+// T(n)= O(n)
+// S(n)= O(1)
 
 // T(n)= O(n)
 // S(n)= O(1)
